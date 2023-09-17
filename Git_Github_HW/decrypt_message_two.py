8e7b0709d0442903dcd5da3f325694ef58157010
hahaha <lige2334@gmail.com>
2023-09-17 18:16:10 -0400

    add code to decrypt_message_two

--- Git_Github_HW/decrypt_message_two.py
+++ Git_Github_HW/decrypt_message_two.py
@@ -5,3 +5,21 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+def decrypt_transposition(message):
+    chars = list(message)  # Convert the string to a list of characters
+    length = len(chars)
+    
+    # Iterate through every second character
+    for i in range(1, length, 2):
+        # If the index from the beginning is >= the index from the end, stop
+        if i >= length - i:
+            break
+
+        # Swap the characters
+        chars[i], chars[length-i-1] = chars[length-i-1], chars[i]
+
+    # Convert back to a string
+    return ''.join(chars)
+
+decrypted_message = decrypt_transposition(encrypted_message)
+print(decrypted_message)
